fa1a4394af28be3ef06bd5aa0b534a4306f3ac3e
saeki <shingen7303@gmail.com>
2019-06-30 10:15:17 +0900

    test

--- db/seeds/development/entries.rb
+++ db/seeds/development/entries.rb
@@ -6,14 +6,11 @@ body =
   "あああああああああああああああああああああああああああああああああああ。"
 
 %w(Taro Jiro Hana).each do |name|
-  member = Member.find_by(name: name)
   0.upto(9) do |idx|
     Entry.create(
-      author: member,
       title: "今日の景色#{idx}",
       body: body,
-      posted_at: 10.days.ago.advance(days: idx),
-      status: %w(draft member_only public)[idx % 3]
+      posted_at: 10.days.ago.advance(days: idx)
     )
   end
 end
